eb6513136d9277364c866d06b2992a455f9126e2
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-06-03 14:09:59 -0500

    Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,55 @@
+ <template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom:10px">
+      <a class="navbar-brand" href="#">
+        <img
+          data-v-ccc41290
+          src="//cdn.shopify.com/s/files/1/1757/1461/files/HZ-no-city-digital-black.png?10146406607426040436"
+          alt="The Rush Market"
+          width="250"
+        />
+      </a>
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'about', params: { admin: false } }" class="nav-link">About Us</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link
+              :to="{ name: 'list', params: { admin: false } }"
+              class="nav-link"
+            >Our Products</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      admin: false
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1 style="margin-top:10px; margin-botton:10px">Rush Market TLC</h1>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p
+          class="text-justify"
+        >This is Rush Market's TLC site. We at the the Rush Market are constantly hunting for fresh finds for our customers, and in the process, sometimes we buy products that are just in need of a little repair that might be beyond what the average person wants to takle. In order to keep our prices at the lowest possible point possible, we can't just take a financial loss on these products, so we post these products here. If you see something that you fancy, then simply click the reserve for market, and then select the date that you will be in the market to pick it up, and talk to a employee and ask to be taken to your product in the backroom. By selecting your product, we will place it in the staging room for you to come see, but if somebody else gets to it first, then we will sell it.</p>
+        <router-link
+          :to="{ name: 'list', params: { admin: false } }"
+          class="btn btn-primary"
+        >Check Out Our Products</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
