ac9c526e171b5c587888c5391a7d0b7e7b2df08d
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-06-11 14:37:49 -0500

    Updated Frontend

--- Working/frontend/src/App.vue
+++ Working/frontend/src/App.vue
@@ -1,55 +1,40 @@
- <template>
+<template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom:10px">
-      <a class="navbar-brand" href="#">
-        <img
-          data-v-ccc41290
-          src="//cdn.shopify.com/s/files/1/1757/1461/files/HZ-no-city-digital-black.png?10146406607426040436"
-          alt="The Rush Market"
-          width="250"
-        />
-      </a>
-      <div>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'about', params: { admin: false } }" class="nav-link">About Us</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link
-              :to="{ name: 'list', params: { admin: false } }"
-              class="nav-link"
-            >Our Products</router-link>
-          </li>
+      <a class="navbar-brand">Nebraska State Legislature Bills</a>
+    </nav>
+    <div class="col-sm">
+        <ul>
+          <ListItem Title="Test"/>
         </ul>
       </div>
-    </nav>
-    <router-view />
   </div>
 </template>
 
-
 <script>
+import ListItem from './components/ListItem.vue'
+
 export default {
-  name: "app",
-  components: {},
+  name: 'App',
+  components: {
+    ListItem
+  },
   data() {
     return {
-      admin: false
+      expand: false,
+      show: true
     };
-  },
-  methods: {}
-};
+  }
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 00px;
 }
 </style>
--- Working/frontend/src/components/ListItem.vue
+++ Working/frontend/src/components/ListItem.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <div class="card" style="width: 18rem;" v-show="show">
-      <img class="card-img-top" :src="Image" />
+    <div class="card" v-show="show">
       <div class="card-body">
         <h5 class="card-title">{{Title}}</h5>
         <p class="card-text" v-show="expand">{{Description}}</p>
-        <h6>${{Price}}</h6>
         <button class="btn btn-primary" @click="expand = true" v-show="!expand">See full description</button>
         <button class="btn btn-primary" @click="expand = false" v-show="expand">Hide description</button>
-        <router-link
-          :to="{ name: 'buy', params: { id: Sku } }"
-          class="btn btn-primary"
-          v-show="expand"
-          style="margin-top: 10px;"
-        >Reserve this Product</router-link>
       </div>
     </div>
   </div>
@@ -25,18 +17,11 @@ export default {
   props: {
     Title: String,
     Description: String,
-    Image: String,
-    Price: Number,
-    Sku: Number
   },
   methods: {
     dele() {
       this.show = false;
-      this.$emit("dele-sku", this.Sku);
     }
-  },
-  created() {
-    this.Admin = this.$route.params.admin;
   },
   data() {
     return {
