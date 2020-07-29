022a33b51e21d1992ee3b3fb852c327244cacd5e
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-07-28 22:16:26 -0500

    Server Connections

---
+++ Working/frontend/src/Server.js
@@ -0,0 +1,41 @@
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {//////////////////////////////////////////////////////////////You will need to change these to the credential for the server
+        user: '',
+        password: '',
+        server: 'localhost', 
+        database: 'Dev' ///////////////////////////////////////////////////////////////////////
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from CombinedTables Where Document=LB310A', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+    console.log('Go to localhost:5000 to check connection')
+});
+
--- Working/frontend/src/main.js
+++ Working/frontend/src/main.js
@@ -3,11 +3,31 @@ import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios';
+
 
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  },
+  template: '<App :info="info" />',
+  render: (h) => h(App)
+  
+})
+
+
+
+
+
