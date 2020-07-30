7586217f2bfbe4e8151a0523f8cb523f5c18c8d0
Joseplh <joemax94@comcast.net>
2020-07-29 21:51:56 -0500

    Update SampleQueries.sql

--- Working/SampleQueries.sql
+++ Working/SampleQueries.sql
@@ -1,1 +1,11 @@
+
+//Selects top 10
 SELECT TOP 10 * from CombinedTable;
+//Selects any document that contains fish in the description
+SELECT Document, Status, Description from CombinedTable where Description LIKE '%fish%';
+//Selects any document proposed by Blood
+SELECT * from CombinedTable where "Primary Introducer"='Blood';
+//Selects Document with fish, before 2020
+SELECT Document, Status, Description, "Update date"
+from CombinedTable 
+where Description LIKE '%fish%' AND ("Update date"<'2020');
