d1683c9cb66315310c6fab677d86a5be0ac18d48
Joseplh <joemax94@comcast.net>
2020-08-14 12:59:14 -0500

    touch-up finals

--- BoxSite/frontend/src/main.js
+++ BoxSite/frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import axios from 'axios';
+
 
 
 Vue.use(BootstrapVue)
@@ -11,23 +11,7 @@ Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  data () {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
-  },
-  template: '<App :info="info" />',
-  render: (h) => h(App)
-  
-})
-
-
-
+  render: h => h(App),
+}).$mount('#app')
 
 
